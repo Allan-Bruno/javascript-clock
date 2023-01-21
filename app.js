46e697ec278d7a54de9f3811308e56c922e9221d
Allan-Bruno <allanbrunooliveiravasconcelos@gmail.com>
2023-01-21 00:30:43 -0300

    new: made the css layout using tailwind

--- app.js
+++
@@ -1,6 +0,0 @@
-const getNewDate = () => {
-  const container = document.getElementById("container");
-  let time = new Date();
-  container.innerText = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
-};
-setInterval(getNewDate, 1000);
